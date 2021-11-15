479973efe4ae9a91ae535c44f0a9bfbd8d9f2e74
Vincent BRAY <develop@vincent-bray.com>
2021-11-15 15:01:45 +0100

    Affichage de nos Tasks dans un Tableau

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -27,7 +27,7 @@ class TaskController extends AbstractController
         // dd($tasks);
 
         return $this->render('task/index.html.twig', [
-            'controller_name' => 'TaskController',
+            'tasks' => $tasks,
         ]);
     }
 }
