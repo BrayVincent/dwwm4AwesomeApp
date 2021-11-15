30d85dc94ece485e83baf127e53c1a532e0433e4
Vincent BRAY <develop@vincent-bray.com>
2021-11-15 14:05:37 +0100

    Récupération de toutes les tâches dans Index()

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -2,9 +2,10 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Task;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class TaskController extends AbstractController
 {
@@ -13,6 +14,18 @@ class TaskController extends AbstractController
      */
     public function index(): Response
     {
+
+        // On va chercher avec Doctrine le Repository de nos Task
+        $repository = $this->getDoctrine()->getRepository(Task::class);
+
+        // Dans ce repository nous récupérons toutes les données
+        $tasks = $repository->findAll();
+
+        // Affichage des données dans le var_dumper
+        // var_dump($tasks);
+        // die;
+        // dd($tasks);
+
         return $this->render('task/index.html.twig', [
             'controller_name' => 'TaskController',
         ]);
