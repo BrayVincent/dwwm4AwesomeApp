f8079cd3aa84c3465697f694a7d62354f1a0b659
Vincent BRAY <develop@vincent-bray.com>
2021-11-16 09:58:55 +0100

    Ajout des catégories

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Tag;
 use App\Entity\Task;
 use Faker\Factory;
 use Doctrine\Persistence\ObjectManager;
@@ -14,6 +15,24 @@ class AppFixtures extends Fixture
         // Création d'un nouvel objet Faker
         $faker = Factory::create('fr_FR');
 
+        // Création de nos 5 catégories
+        for ($c = 0; $c < 5; $c++) {
+            // Création d'un nouvel objet Tag
+            $tag = new Tag;
+
+            // On ajoute un nom à notre catégorie
+            $tag->setName($faker->colorName());
+
+            // On fait persister les données
+            $manager->persist($tag);
+        }
+
+        // On push les catégories en BDD
+        $manager->flush();
+
+        // Récupération des catégories créées
+        $tags = $manager->getRepository(Tag::class)->findAll();
+
         // Création entre 15 et 30 tâches aléatoirement
         for ($t = 0; $t < mt_rand(15, 30); $t++) {
 
@@ -24,7 +43,8 @@ class AppFixtures extends Fixture
             $task->setName($faker->sentence(6))
                 ->setDescription($faker->paragraph(3))
                 ->setCreatedAt(new \DateTime()) // Attention les dates sont fonctions du paramétrage du serveur
-                ->setDueAt($faker->dateTimeBetween('now', '6 months'));
+                ->setDueAt($faker->dateTimeBetween('now', '6 months'))
+                ->setTag($faker->randomElement($tags));
 
             // On fait persister les données
             $manager->persist($task);
