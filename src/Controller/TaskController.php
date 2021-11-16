cbceff573bc3b75d116dce0e9179bb94210bf253
Vincent BRAY <develop@vincent-bray.com>
2021-11-16 11:54:05 +0100

    CreateTask()

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -13,7 +13,7 @@ class TaskController extends AbstractController
 {
 
     /**
-     * @Route("/task/listing", name="task")
+     * @Route("/task/listing", name="task_listing")
      */
     public function index(): Response
     {
@@ -42,7 +42,24 @@ class TaskController extends AbstractController
 
         $task = new Task;
 
+        $task->setCreatedAt(new \DateTime());
+
         $form = $this->createForm(TaskType::class, $task, []);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // $task->setName($form['name']->getData())
+            //     ->setDescription($form['description']->getData())
+            //     ->setDueAt($form['dueAt']->getData())
+            //     ->setTag($form['tag']->getData());
+
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($task);
+            $manager->flush();
+
+            return $this->redirectToRoute('task_listing');
+        }
 
         return $this->render('task/create.html.twig', [
             'form' => $form->createView()
