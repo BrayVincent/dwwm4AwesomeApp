0631cc4ee14d49b8113b28281da1c9f7900570ba
Vincent BRAY <develop@vincent-bray.com>
2021-11-24 11:55:58 +0100

    access denied hander

---
+++ src/Security/AccessDeniedHandler.php
@@ -0,0 +1,18 @@
+<?php
+// src/Security/AccessDeniedHandler.php
+namespace App\Security;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Security\Http\Authorization\AccessDeniedHandlerInterface;
+
+class AccessDeniedHandler implements AccessDeniedHandlerInterface
+{
+public function handle(Request $request, AccessDeniedException $accessDeniedException): ?Response
+{
+// ...
+
+return new Response('bundles/TwigBundle/Exception/error403.html.twig', 403);
+}
+}
