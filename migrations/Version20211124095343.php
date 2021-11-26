835cd887acf574e349cecb1a2598ccb2c06a4e46
Vincent BRAY <develop@vincent-bray.com>
2021-11-26 10:52:02 +0100

    Adapt Migration

--- migrations/Version20211124095343.php
+++ migrations/Version20211124095343.php
@@ -20,7 +20,9 @@ final class Version20211124095343 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('SET FOREIGN_KEY_CHECKS=0');
         $this->addSql('ALTER TABLE task CHANGE user_id user_id INT NOT NULL');
+        $this->addSql('SET FOREIGN_KEY_CHECKS=1');
     }
 
     public function down(Schema $schema): void
